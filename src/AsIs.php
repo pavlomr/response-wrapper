2d3671c3a50f809c8860a50982838145bef572cc
Pavlo Marenyuk <pavlomr@gmail.com>
2022-05-04 15:58:04 +0300

    Split NoWrap and AsIs

--- src/AsIs.php
+++ src/AsIs.php
@@ -7,8 +7,18 @@ use Psr\Http\Message\ResponseInterface;
 class AsIs implements ResponseWrapperInterface
 {
 
+    /**
+     * @var \Psr\Http\Message\ResponseInterface
+     */
+    private ResponseInterface $response;
+
+    public function __construct(ResponseInterface $buffer)
+    {
+        $this->response = $buffer;
+    }
+
     public function wrap(ResponseInterface $response): ResponseInterface
     {
-        return $response;
+        return $this->response;
     }
 }
